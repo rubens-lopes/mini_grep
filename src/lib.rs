867a086d6d5f462fab189677b645843c8d223568
Rubens Armôa Lopes <github@rubenslop.es>
2022-10-17 19:25:20 -0400

    implementa pesquisa mais permissiva

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error, fs};
+use std::{env, error, fs};
 
 pub struct Configuração {
     pub consulta: String,
     pub caminho_arquivo: String,
+    pub modo_permissivo: bool,
 }
 
 impl Configuração {
@@ -11,18 +12,31 @@ impl Configuração {
             return Err(String::from("Argumentos insuficientes"));
         }
 
+        let modo_permissivo = env::var("MODO_PERMISSIVO").is_ok();
+
         Ok(Configuração {
             consulta: argumentos[1].clone(),
             caminho_arquivo: argumentos[2].clone(),
+            modo_permissivo,
         })
     }
 }
 
 pub fn executar(configuração: Configuração) -> Result<(), Box<dyn error::Error>> {
-    let Configuração { consulta, caminho_arquivo } = configuração;
+    let Configuração {
+        consulta,
+        caminho_arquivo,
+        modo_permissivo,
+    } = configuração;
 
     let conteúdo = fs::read_to_string(caminho_arquivo)?;
-    for linha in pesquisar(&consulta, &conteúdo) {
+    let resultado = if modo_permissivo {
+        pesquisar_permissivamente(&consulta, &conteúdo)
+    } else {
+        pesquisar(&consulta, &conteúdo)
+    };
+
+    for linha in resultado {
         println!("{linha}")
     }
 
@@ -43,21 +57,59 @@ pub fn pesquisar<'a>(consulta: &str, conteúdo: &'a str) -> Vec<&'a str> {
     resultado
 }
 
+pub fn pesquisar_permissivamente<'a>(consulta: &str, conteúdo: &'a str) -> Vec<&'a str> {
+    let consulta = &preparar_texto(&consulta);
+    let mut resultado = Vec::new();
+
+    for linha in conteúdo.lines() {
+        if preparar_texto(linha).contains(consulta) == false {
+            continue;
+        }
+
+        resultado.push(linha);
+    }
+
+    resultado
+}
+
+fn preparar_texto(texto: &str) -> String {
+    diacritics::remove_diacritics(&texto.to_lowercase())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn um_resultado() {
-        let consulta = "duti";
+    fn pesquisa_estrita() {
+        let consulta = "gur";
         let conteúdo = "\
 Rust:
 seguro, rápido, produtivo.
-Escolha três.";
+Escolha três.
+Gurus recomendam.";
 
         assert_eq!(
             vec!["seguro, rápido, produtivo."],
             pesquisar(consulta, conteúdo)
+        );
+    }
+
+    #[test]
+    fn pesquisa_permissiva() {
+        let consulta = "aP";
+        let conteúdo = "\
+Rust:
+seguro, rápido, produtivo.
+Escolha três.
+Escreva aplicações que rodam em qualquer lugar.";
+
+        assert_eq!(
+            vec![
+                "seguro, rápido, produtivo.",
+                "Escreva aplicações que rodam em qualquer lugar."
+            ],
+            pesquisar_permissivamente(consulta, conteúdo)
         );
     }
 }
