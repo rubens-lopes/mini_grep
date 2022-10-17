bbb7a682c814357d30d690fa319af1666d86c147
Rubens Armôa Lopes <github@rubenslop.es>
2022-10-16 20:08:06 -0400

    isolando responsabilidades

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ use std::{env, fs};
 
 fn main() {
     let argumentos: Vec<String> = env::args().collect();
+    let configuração = Configuração::from(&argumentos);
 
-    let consulta = &argumentos[1];
-    let caminho_arquivo = &argumentos[2];
-    
-    println!("Buscando por {}", consulta);
-    println!("No arquivo {}", caminho_arquivo);
+    println!("Buscando por {}", configuração.consulta);
+    println!("No arquivo {}", configuração.caminho_arquivo);
 
-    let conteúdo = fs::read_to_string(caminho_arquivo)
-        .expect("Deve conseguir ler o arquivo");
+    let conteúdo =
+        fs::read_to_string(configuração.caminho_arquivo).expect("Deve conseguir ler o arquivo");
 
     println!("No texto:\n{conteúdo}");
+}
+
+struct Configuração {
+    consulta: String,
+    caminho_arquivo: String,
+}
+
+impl Configuração {
+    fn from(argumentos: &[String]) -> Self {
+        Configuração {
+            consulta: argumentos[1].clone(),
+            caminho_arquivo: argumentos[2].clone(),
+        }
+    }
 }
