5a01465992907fe682b87e0a4e8184d7510f5bc8
Rubens Armôa Lopes <github@rubenslop.es>
2022-10-17 19:32:27 -0400

    exibe erros na saída correta

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use mini_grep::Configuração;
 fn main() {
     let argumentos: Vec<String> = env::args().collect();
     let configuração = Configuração::from(&argumentos).unwrap_or_else(|erro| {
-        println!("Problema ao obter argumentos: {erro}");
+        eprintln!("Problema ao obter argumentos: {erro}");
         process::exit(1);
     });
 
     if let Err(erro) = mini_grep::executar(configuração) {
-        println!("Aplicação falhou: {erro}");
+        eprintln!("Aplicação falhou: {erro}");
         process::exit(1);
     }
 }
